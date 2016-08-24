526cf817da3412739ef4ca74e8234512d0bd3cfb
saskcow <callum@tarttelin.co.uk>
2016-08-24 17:18:54 +0100

    stack adds rectangles

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -1,7 +1,10 @@
-
+import Phaser from 'phaser';
 
 export default class Stack {
   constructor (x, y, width, height) {
-    this.zones = [];
+    const boxHeight = height/3;
+    this.zones = [new Phaser.Rectangle(x, y, width, boxHeight),
+      new Phaser.Rectangle(x, y + boxHeight, width, boxHeight),
+      new Phaser.Rectangle(x, y + boxHeight + boxHeight, width, boxHeight)];
   }
 }
